8495dbca48bb45ebefb50f10c0ba4cc5f2d0b9bb
guillermina <guilleves@gmail.com>
2017-09-16 02:28:54 -0300

    switch to postgresql

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Events
+module Exercise_170916_guillerminavescovo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
