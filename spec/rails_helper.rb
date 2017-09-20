678085c197dce37b22866dfca0a3730de4a6479f
guillermina <guilleves@gmail.com>
2017-09-20 13:57:51 -0300

    More CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'factory_girl_rails'
-require 'support/factory_girl'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -32,7 +31,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include FactoryGirl::Syntax::Methods
 
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
